40564ab49599d3fe5810f8b5e2007b33bfb4a612
tomchar1 <146212970+tomchar1@users.noreply.github.com>
2024-04-10 10:09:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+// Functionize Minecraft Fishing Start Code
+
+// Html variables
+let resultImg = document.getElementById("result-img");
+let numCodSpan = document.getElementById("num-cod");
+let numSalmonSpan = document.getElementById("num-salmon");
+let numTropicalSpan = document.getElementById("num-tropical");
+let numPufferSpan = document.getElementById("num-puffer");
+// Fish count variable
+let numCod = 0;
+let numSalmon = 0;
+let numTropical = 0;
+let numPuffer = 0;
+
+// Fish Button Event Listener
+document.getElementById("fish-btn").addEventListener("click", fishBtnClicked);
+
+function fishBtnClicked() {
+  // Determin selected charecter
+  let charecter = document.getElementById("character-select").value;
+
+  if (charecter === "steve") {
+    // steve Fish Simulator
+    let randNum = Math.random();
+    if (randNum < 0.7) {
+      resultImg.src = "img/Raw-Cod.png";
+      numCod++;
+      numCodSpan.innerHTML = numCod;
+    } else if (randNum < 0.9) {
+      resultImg.src = "img/Raw-Salmon.png";
+      numSalmon++;
+      numSalmonSpan.innerHTML = numSalmon;
+    } else if (randNum < 0.95) {
+      resultImg.src = "img/Tropical-Fish.png";
+      numTropical++;
+      numTropicalSpan.innerHTML = numTropical;
+    } else {
+      resultImg.src = "img/Pufferfish.png";
+      numPuffer++;
+      numPufferSpan.innerHTML = numPuffer;
+    }
+  } else if (charecter === "alex") {
+    // Fish with alex
+    let randNum = Math.random();
+    if (randNum < 0.1) {
+      resultImg.src = "img/Raw-Cod.png";
+      numCod++;
+      numCodSpan.innerHTML = numCod;
+    } else if (randNum < 0.2) {
+      resultImg.src = "img/Raw-Salmon.png";
+      numSalmon++;
+      numSalmonSpan.innerHTML = numSalmon;
+    } else if (randNum < 0.5) {
+      resultImg.src = "img/Tropical-Fish.png";
+      numTropical++;
+      numTropicalSpan.innerHTML = numTropical;
+    } else {
+      resultImg.src = "img/Pufferfish.png";
+      numPuffer++;
+      numPufferSpan.innerHTML = numPuffer;
+    }
+  } else if (charecter === "villager") {
+    // Fish with alex
+    let randNum = Math.random();
+    if (randNum < 0.25) {
+      resultImg.src = "img/Raw-Cod.png";
+      numCod++;
+      numCodSpan.innerHTML = numCod;
+    } else if (randNum < 0.5) {
+      resultImg.src = "img/Raw-Salmon.png";
+      numSalmon++;
+      numSalmonSpan.innerHTML = numSalmon;
+    } else if (randNum < 0.75) {
+      resultImg.src = "img/Tropical-Fish.png";
+      numTropical++;
+      numTropicalSpan.innerHTML = numTropical;
+    } else {
+      resultImg.src = "img/Pufferfish.png";
+      numPuffer++;
+      numPufferSpan.innerHTML = numPuffer;
+    }
+  }
+
+  // Alex Fish Simulator
+}
